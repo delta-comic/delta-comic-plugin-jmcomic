9b3f4a17b16df97a6161d1e36620958128d814a9
w enxig <wenxinguo12@yeah.net>
2026-05-23 21:47:11 +0800

    chore: try github

--- packages/plugin/src/symbol.ts
+++ packages/plugin/src/symbol.ts
@@ -3,4 +3,4 @@ export const pluginName = 'jmcomic'
 import { declareDepType } from '@delta-comic/plugin'
 import type { LayoutLib } from 'delta-comic-plugin-layout'
 
-export const layoutModule = declareDepType<LayoutLib>('layout')
+export const layoutModule = declareDepType<LayoutLib>('layout') as any
